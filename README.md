869be45c5d2e73a3f5679cffe5d75f6c0265fa09
Webs95 <93885891+Webs95@users.noreply.github.com>
2023-05-30 21:29:20 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# popover-for-map-protorype
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-afdayr)
---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { ExampleComponent } from './example.component';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot([]), IonicModule.forRoot({ mode: 'ios' })],
+  declarations: [AppComponent, ExampleComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
