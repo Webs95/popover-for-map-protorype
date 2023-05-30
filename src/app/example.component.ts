9fc15f80d52c4c8966e4146c748e9288b4490268
Webs95 <93885891+Webs95@users.noreply.github.com>
2023-05-30 21:32:31 +0300

    refactor: change css to scss

--- src/app/example.component.ts
+++ src/app/example.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-example',
   templateUrl: 'example.component.html',
-  styleUrls: ['example.component.css']
+  styleUrls: ['example.component.scss']
 })
 export class ExampleComponent { }
